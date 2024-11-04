1c2e94de273d4283687581dad664eb74111ca642
sansaovieira <vieirasansao42@gmail.com>
2024-11-04 14:37:43 -0300

    feat(day 10 - activities): Solve the activities of day 10

---
+++ src/day10/ArrayListAdicionandoCores.java
@@ -0,0 +1,27 @@
+package day10;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ArrayListAdicionandoCores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		ArrayList<String> cores = new ArrayList<String>();
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Digite o nome da " + (i + 1 + "°") + " cor: " );
+			cores.add(scanner.nextLine());
+		}
+		
+		System.out.println("Listar todas as cores: " + cores);
+		Collections.sort(cores);
+		System.out.println("\nOrdenar as cores: " + cores);
+		
+		scanner.close();
+	}
+	
+}
---
+++ src/day10/CollectionSetSolicitarDadosAoUser.java
@@ -0,0 +1,30 @@
+package day10;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class CollectionSetSolicitarDadosAoUser {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		HashSet<Integer> numerosInteiros = new HashSet<Integer>();
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("\nDigite o " + (i + 1 + "°") + "número");		
+			int numeroDigitado = scanner.nextInt();
+			if(!numerosInteiros.add(numeroDigitado)) {
+				System.out.println("\nEste número já foi inserido! \nPor favor, digite um número não repetido");
+			}
+		}
+		
+		System.out.println("Listar dados do set: ");
+		Iterator<Integer> iterator = numerosInteiros.iterator();
+		
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
+
+}
